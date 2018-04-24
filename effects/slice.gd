292e34b8c722a12dcf866d4f17867f01222713d5
StrikebyClaymore <36325557+StrikebyClaymore@users.noreply.github.com>
2018-04-24 21:08:36 +0300

    fixed sound effect
    Oh. Alright

--- effects/slice.gd
+++ effects/slice.gd
@@ -3,8 +3,8 @@ extends Sprite
 var slide_x = Vector2(10, -2)
 
 func _ready():
+	$sound.set_volume_db(-20) 
 	$Animation.play("attack")
-	$sound.set_volume_db(-10) 
 
 func _process(delta):
 	if visible == false and $Animation.stop():

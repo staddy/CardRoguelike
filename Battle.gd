de52c2cc3646d91a0ef6e534fb6047fe916d8b99
StrikebyClaymore <36325557+StrikebyClaymore@users.noreply.github.com>
2018-04-24 20:55:00 +0300

    attached sound to basic attack

--- Battle.gd
+++ Battle.gd
@@ -3,6 +3,9 @@ extends Node2D
 var turn = -1
 var Card = preload("res://Card.tscn")
 
+var effectsMas = []
+var effects = preload("res://effects/Effects.tscn")
+
 # holds cards ids
 var draw_pile = []
 # auto managed list of cards on screen
@@ -19,6 +22,12 @@ var selected_enemy = null
 
 var enemy_turn = false
 var current_enemy = 0
+
+func add_classes():
+	var e = effects.instance()
+	for i in e.get_children().size():
+		if e.get_child(i).is_in_group("effect"):
+			effectsMas.append(e.get_child(i))
 
 var hp = 10 setget set_hp
 func set_hp(value):
@@ -73,6 +82,7 @@ func _ready():
 	self.max_mana = global.current_max_mana
 	draw_pile = global.shuffle_list(global.deck)
 	new_turn()
+	add_classes()
 	pass
 
 func draw_cards():
@@ -122,6 +132,9 @@ func play_card(card):
 	
 	if card.type == "attack":
 		selected_enemy.damage(card.value)
+		var e = effectsMas[0].duplicate()
+		e.position = selected_enemy.position
+		add_child(e)
 	elif card.type == "skill":
 		if card.effect == "block":
 			self.block += card.value
--- effects/slice.gd
+++ effects/slice.gd
@@ -4,10 +4,11 @@ var slide_x = Vector2(10, -2)
 
 func _ready():
 	$Animation.play("attack")
+	$sound.set_volume_db(-10) 
 
 func _process(delta):
-	if visible == false:
-			queue_free()
+	if visible == false and $Animation.stop():
+		queue_free()
 
 func _on_Timer_timeout():
 	position += slide_x

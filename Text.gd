d6cc01420628b9057a28ee0030ed593a37622b4c
Stanislav <stas.ponkrashov@gmail.com>
2018-06-16 03:17:42 +0300

    Chinese room effect

--- Text.gd
+++ Text.gd
@@ -3,7 +3,7 @@ extends Node2D
 var SCREEN_WIDTH = 960#ProjectSettings.get_setting("display/window/width")
 var SCREEN_HEIGHT = 540#ProjectSettings.get_setting("display/window/height")
 
-var letters = ["吧","爸","八","百","北","不","田","由","甲","申","甴","电","甶","男","甸","甹","町","画","甼","甽","甾","甿","畀","畁","畂","畃","畄","畅","畆","畇","畈","畉","畊","畋","界","畍","畎","畏","畐","畑"]
+var letters = ["吧","爸","八","百","北","不","田","由","甲","申","甴","电","甶","男","甸","甹","町","画","甼","甽","甾","甿","畀","畁","畂","畃","畄","畅","畆","畇","畈","畉","畊","畋","界","畍","畎","畏","畐","畑", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "S", "U", "P", "R", "E", "M", "E", "S", "E", "N", "S", "E"]
 var font = DynamicFont.new()
 
 var time = 0
@@ -12,17 +12,15 @@ func _ready():
 	var data = DynamicFontData.new()
 	data.font_path = "res://Zpix.ttf"
 	font.font_data = data
-	font.size = 11
+	font.size = 22
 
 func _draw():
-	pass
-	for i in range(SCREEN_WIDTH / 11):
-		for j in range(SCREEN_HEIGHT / 11):
+	for i in range(SCREEN_WIDTH / 22 + 1):
+		for j in range(SCREEN_HEIGHT / 22 + 1):
 			if randf() > 0:#.999:
-				draw_string(font, Vector2(i * 11, 11 + j * 11), letters[randi() % letters.size()], Color("FF00FFFF"))
+				draw_string(font, Vector2(i * 22, 22 + j * 22), letters[randi() % letters.size()], Color("0900FFFF"))
 
 func _process(delta):
-	pass
 	time += delta
 	if time > 0.1:
 		update()
